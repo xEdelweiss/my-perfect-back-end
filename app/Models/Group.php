733061130561ae14c00b36823a9cf323c5e8ecbd
Michael Sverdlikovsky <xedelweiss@gmail.com>
2015-12-18 18:13:26 +0200

    Update phpDoc for models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Group
+ *
+ * @property integer $id
+ * @property integer $owner_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read User $owner
+ * @property-read \Illuminate\Database\Eloquent\Collection|User[] $users
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Group whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Group whereOwnerId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Group whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Group whereUpdatedAt($value)
+ */
 class Group extends Model
 {
     /**
